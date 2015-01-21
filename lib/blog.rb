0355cc6aff9bb4351618ca28921644d13399128b
xarisd <xaris.dimitriou@gmail.com>
2015-01-21 09:32:42 +0200

    Fix typos and get the required gems

--- lib/blog.rb
+++ lib/blog.rb
@@ -1,13 +1,14 @@
 require 'sinatra/base'
 require 'ostruct'
 require 'time'
+require 'yaml'
 
 class Blog < Sinatra::Base
   set :root, File.expand_path('../../', __FILE__)
   set :articles, []
 
   Dir.glob "#{root}/articles/*.md" do |file|
-    meta, content = File.read(file).split "\n\n", 2)
+    meta, content = File.read(file).split("\n\n", 2)
     article = OpenStruct.new YAML.load(meta)
     article.date = Time.parse(article.date.to_s)
     article.content = content
@@ -27,5 +28,7 @@ class Blog < Sinatra::Base
     erb :index
   end
 
+
+  run! if __FILE__ == $0
 end
-  
+
